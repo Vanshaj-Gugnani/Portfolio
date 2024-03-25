6826845c10456156fb0a8cc25c9f3c53a4d6be6a
vgugnani <vgugnani@myseneca.ca>
2024-03-25 19:51:59 -0400

    Skills animation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var scrollEventHandler = function()
-{
-  window.scroll(0, window.scrollY)
-}
-
-window.addEventListener("scroll", scrollEventHandler, false);
+window.addEventListener('mousemove',(e)=>{
+  document.querySelectorAll('.img').forEach((img) =>{
+    let speed = parseInt(img.getAttribute('data-speed'));
+    xmove = e.clientX*speed/300;
+    ymove = e.clientY*speed/300;
+    console.log(xmove)
+    img.style.transform = `translateX(${xmove}px) translateY(${ymove}px)`
+  })
+})
