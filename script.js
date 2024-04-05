7c664d448ccaf3ec6f52bfb19608058eb881bd9b
vgugnani <vgugnani@myseneca.ca>
2024-04-05 11:40:26 -0400

    scroll animation

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ window.addEventListener('mousemove',(e)=>{
     img.style.transform = `translateX(${xmove}px) translateY(${ymove}px)`
   })
 })
+
+var timeline = gsap.timeline({
+  scrollTrigger:{
+    trigger: '.page3',
+    start: 'top top',
+    bottom: 'bottom top',
+    pin: true,
+    scrub: 0.5
+  }
+});
+
+timeline
+  .to('.subpage1', {
+    top: '-50%',
+    duration: 2,
+    delay:2,
+  }, 'a')
+  .to('.subpage2',{
+    bottom: '-50%',
+    duration: 2,
+    delay:2,
+  }, 'a')
+  .to('.img',{
+    opacity: 0,
+    display:'none',
+    duration:1,
+    delay:1,
+  }, 'a')
+  ;
